9a5e45af4ac26652fec3bf0133d644fbf9ab2eed
Matthew Webb <rybka.webb@gmail.com>
2015-07-13 18:13:36 +0100

    Mobile notification tests

--- app/app.js
+++ app/app.js
@@ -1,13 +1,5 @@
 "use strict";
 
+var _roost = _roost || [];
 
-	var _roost = _roost || [];
-    _roost.push(['onresult', function(data) {
-        // your code goes here, in the callback function
-        console.log("onresult", data.tags);
-    }]);
-
-	
-	//* Roost Push Notification Handlder //
-	angular.module('app', ['ui.bootstrap']);
-
+angular.module('app', ['ui.bootstrap']);
